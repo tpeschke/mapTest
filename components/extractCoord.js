a03a6d7fbf7ce1033975303f718dc8894fc8db17
Trent <mr.peschke@gmail.com>
2018-05-15 09:12:59 -0600

    debugging the non-curving road

--- components/extractCoord.js
+++ components/extractCoord.js
@@ -1,6 +1,6 @@
 module.exports = {
     extractCoord: function (obj,start,end) {
-        console.log("starting", obj.length)
+        console.log("starting", obj)
         tempArr = []
         obj.forEach(val => {
             tempArr.push({latitude: val.start_location.lat, longitude: val.start_location.lng}, {latitude: val.end_location.lat, longitude: val.end_location.lng})
@@ -8,7 +8,7 @@ module.exports = {
 
         if (tempArr[0].latitude !== start.latitude || tempArr[0].longitude !== start.longitude) tempArr.unshift(start)
         if (tempArr[tempArr.length-1].latitude !== end.latitude || tempArr[tempArr.length-1].longitude !== end.longitude) tempArr.push(end)
-        console.log("ending", tempArr)
+        // console.log("ending", tempArr.length)
             return tempArr    
         }
 }
